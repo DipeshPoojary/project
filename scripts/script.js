5ac9731c7af350035227e4d6c3f9f26c8e3eb461
DipeshPoojary <dipeshjpoojary20@gmail.com>
2024-12-23 07:27:27 +0530

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+/* Path: scripts/main.js */
+// JavaScript Code
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('Thank you for clicking Learn More!');
+});
